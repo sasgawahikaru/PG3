0c3ee8570303518d475ba85a7745f157b120b788
sasagawahikaru <k021g1383@g.neec.ac.jp>
2022-11-29 13:10:21 +0900

    2_1 多分完成

--- main.cpp
+++ main.cpp
@@ -3,161 +3,128 @@
 #include<time.h>
 #include<windows.h>
 #include<functional>
+#include<vector>
+#include<list>
+#include<string>
+using namespace std;
 
-//int i;
-//typedef void (*PFunc)(int*);
-//
-//void callback(int* s)
-//{
-//	srand((unsigned int)time(NULL));
-//	int num = rand() % 6 + 1;
-//	printf("%d\n", num);
-//	if (i % 2 == 0)
-//	{
-//		if (num % 2 == 0)
-//		{
-//			printf("偶数\n");
-//			printf("当たり");
-//		}
-//		else
-//		{
-//			printf("奇数\n");
-//			printf("外れ");
-//		}
-//	}
-//	if (i % 2 == 1)
-//	{
-//		if (num % 2 == 0)
-//		{
-//			printf("偶数\n");
-//			printf("外れ");
-//		}
-//		else
-//		{
-//			printf("奇数\n");
-//			printf("当たり");
-//		}
-//	}
-//}
-//void setTimeout(PFunc p, int second)
-//{
-//	Sleep(second * 1000);
-//
-//	p(&second);
-//}
+int main()
+{
 
-typedef void (*PFunc)(int*);
-
-
-//void callback(int* s) {
-	int main(int argc, const char* argv[])
-	{
-		int a;
-		printf("偶数だと思うなら偶数値を 奇数だと思うなら奇数値を入力してENTER\n");
-		scanf_s("%d", &a);
-			srand((unsigned int)time(NULL));
-
-			std::function<int()>saikoro = []() {return rand() % 6 + 1; };
-			int num = saikoro();
-			printf("%d\n", num);
-			if (a % 2 == 0)
-			{
-				if (num % 2 == 0)
-				{
-					printf("偶数\n");
-					printf("当たり");
-				}
-				else
-				{
-					printf("奇数\n");
-					printf("外れ");
-				}
-			}
-			if (a % 2 == 1)
-			{
-				if (num % 2 == 0)
-				{
-					printf("偶数\n");
-					printf("外れ");
-				}
-				else
-				{
-					printf("奇数\n");
-					printf("当たり");
-				}
-			}
-		return 0;
-	}
-	//void setTimeout(PFunc p, int second)
+	//std::list<int>list{ 1,3,5,7,10 };
+	//for (std::list<int>::iterator itr = list.begin(); itr != list.end(); ++itr)
 	//{
-	//	Sleep(second * 1000);
-
-	//	p(&second);
+	//	if (*itr == 3) {
+	//		itr = list.insert(itr, 0);
+	//		++itr;
+	//	}
+	//	printf("%d\n", *itr);
 	//}
+	//printf("%d\n", list);
 
+	std::list<const char *>stations =
+	{
+		"Tokyo","Kanda","Akihabara",
+		"Okachimachi","Ueno","Uguisudani",
+		"Nippori","Tabata",
+	};
 
-//typedef void (*PFunc)(int*);
-//
-//int i;
-//void callback(int* s)
+	printf("--- 1970 ---\n");
+	for (auto itr = stations.begin(); itr != stations.end(); itr++)
+	{
+		printf("%s\n", *itr);
+	}
+	for (auto itr = stations.begin(); itr != stations.end(); itr++)
+	{
+		//if (*itr == "Tabata")
+		if (strcmp( *itr,"Tabata")==0)
+		{
+			stations.insert(itr, "Nishi-Nippori");
+			break;
+		}
+	}
+	/////////////////
+	//1971年"Nishi-Nippori"追加
+	//"tabata"を探し追加する
+	printf("--- 2019 ---\n");
+	for (auto itr = stations.begin(); itr != stations.end(); ++itr)
+	{
+		printf("%s\n", *itr);
+	}
+	for (auto itr = stations.begin(); itr != stations.end(); itr++)
+	{
+		//if (*itr == "Tabata")
+		if (strcmp(*itr, "Tokyo") == 0)
+		{
+			stations.push_front("Yurakutyou");
+			stations.push_front("Shimbashi");
+			stations.push_front("Hamamatsucho");
+			stations.push_front("Tamachi");
+			break;
+		}
+	}
+	for (auto itr = stations.begin(); itr != stations.end(); ++itr)
+	{
+		if (strcmp(*itr, "Tamachi") == 0)
+		{
+			stations.insert(itr, "Takanawa-Gateway");
+		}
+	}
+	/////////////////
+	//2020"Takanawa-Gateway"追加
+	//"Tamachi"を探し追加する
+	printf("--- 2022 ---\n");
+	for (auto itr = stations.begin(); itr != stations.end(); itr++)
+	{
+		printf("%s\n", *itr);
+	}
+	return 0;
+}
+
+/*vector<int>e = { 1,2,3 };
+
+vector<int>::iterator it;
+
+it = e.begin();
+
+cout << *it << endl;
+
+it++;
+
+cout << *it << endl;*/
+
+//std::list<int>lst(33, 4);
+//std::list<int>::iterator itr;
+//itr = lst.begin();
+//itr = lst.end();
+//itr--;
+//itr--;
+
+//std::list<int>lst;
+//for (auto itr = lst.begin(); itr != lst.end(); ++itr)
 //{
-//	for (int a = 0; a < 1; a++)
-//		srand((unsigned int)time(NULL));
+//	std::cout << *itr << "\n";
+//}
+
+//list<int>lst{ 114,514,364 };
+//lst.push_front(7);
+//lst.push_back(777);
+//for (int i = 0; i < 5; i++)
+//{
+//	lst.push_front(i);
+//}
+//for (int i = 0; i < 5; i++)
+//{
+//	lst.push_back(i);
+//}
+
+//list<int>list{ 1,3,5,7,10 };
+//for (std::list<int>::iterator itr = list.begin(); itr != list.end(); ++itr)
+//{
+//	if (*itr == 3)
 //	{
-//		int num = rand() % 6 + 1;
-//		printf("%d\n", num);
-//		if (i % 2 == 0)
-//		{
-//			if (num % 2 == 0)
-//			{
-//				printf("偶数\n");
-//				printf("当たり");
-//			}
-//			else
-//			{
-//				printf("奇数\n");
-//				printf("外れ");
-//			}
-//		}
-//		if (i % 2 == 1)
-//		{
-//			if (num % 2 == 0)
-//			{
-//				printf("偶数\n");
-//				printf("外れ");
-//			}
-//			else
-//			{
-//				printf("奇数\n");
-//				printf("当たり");
-//			}
-//		}
+//		itr = list.insert(itr, 0);
+//		++itr;
 //	}
 //}
-//void setTimeout(PFunc p, int second)
-//{
-//	Sleep(second * 1000);
-//
-//	p(&second);
-//}
-//int main(int argc, const char* argv[]) {
-//
-//	std::function<int(int)>fx = [&](int i) {return i + 1; };
-//	auto fx2 = [&](int i) {return i + 1; };
-//
-//	[]() {printf("偶数だと思うなら偶数値を 奇数だと思うなら奇数値を入力してENTER\n"); }();
-//	scanf_s("%d", &i);
-//	PFunc p;
-//	p = callback;
-//	setTimeout(p, 3);
-//	return 0;
-//
-//}
-//int main()
-//{
-//
-//	PFunc p;
-//	p = callback;
-//	setTimeout(p, 3);
-//	return 0;
-//}
+//printf("%f", list);
